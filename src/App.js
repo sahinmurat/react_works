6df7d1c1fe64f7a31cc5b6eba8697a2a0a6738ef
Murat Sahin <yazilimogren1111@gmail.com>
2021-03-02 14:28:27 +0100

    map function and usage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
   constructor(){
     super();
     this.state = {
-      string: 'Hello Murat'
+      monsters: [
+        {
+          name:'Frankenstein',
+          id: 'adse'
+        },
+        {
+          name:'Dracula',
+          id: 'adsde'
+        },
+        {
+          name:'Zombie',
+          id: 'ads2fe'
+        },
+
+      ]
     }
   }
   render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         {this.state.string}
-        </p>
-        <button onClick={()=> this.setState({string : 'Hey Murat, how are you?'})}> Change the Text</button>
-      </header>
+     {this.state.monsters.map(monster => (
+       <h1 key={monster.id}> {monster.name} </h1>
+     ) )}
     </div>
   );
 }
